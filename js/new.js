7f80657bb3ff5dbe2224ef4b88109298126c4dea
Aswinshaji0001 <Aswinshaji0001@gmail.com>
2024-07-11 09:27:38 +0530

    Added

---
+++ js/new.js
@@ -0,0 +1,36 @@
+
+a=0;
+sum=0;
+    // do
+    // {
+    //     if(a%2==1)
+    //         {
+    //             console.log( "The odd numbers are "+a);
+    //         }
+    //     a++;
+        
+    // }while(a<=10);
+
+    for(a=0;a<=10;a++)
+
+        {
+           sum=sum+a;
+            
+        }
+        console.log(sum);
+
+    // for(a=1;a<=10;a++)
+
+    //     {
+    //         if(a%2==0)
+    //             {
+    //                 console.log("The even numbers are "+a);
+    //             }
+            
+    //     }
+    
+    b=0;
+    c=1;
+for(b=0;b<10;b++){
+    sum=
+}
