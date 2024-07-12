27dc716e0a28ebe12bc8d89b19b7ab1a333f1b6d
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-07-12 08:53:59 +0530

    Added

--- js/new.js
+++ js/new.js
@@ -1,15 +1,15 @@
 
 a=0;
 sum=0;
-    // do
-    // {
-    //     if(a%2==1)
-    //         {
-    //             console.log( "The odd numbers are "+a);
-    //         }
-    //     a++;
+    do
+    {
+        if(a%2==1)
+            {
+                console.log( "The odd numbers are "+a);
+            }
+        a++;
         
-    // }while(a<=10);
+    }while(a<=10);
 
     for(a=0;a<=10;a++)
 
@@ -19,18 +19,47 @@ sum=0;
         }
         console.log(sum);
 
-    // for(a=1;a<=10;a++)
+    for(a=1;a<=10;a++)
 
-    //     {
-    //         if(a%2==0)
-    //             {
-    //                 console.log("The even numbers are "+a);
-    //             }
+        {
+            if(a%2==0)
+                {
+                    console.log("The even numbers are "+a);
+                }
             
-    //     }
+        }
     
-    b=0;
-    c=1;
-for(b=0;b<10;b++){
-    sum=
-}
+    let b=0;
+    let c=1;
+    let d;
+    console.log(b);
+    console.log(c);
+        for( let i=0;i<=10;i++)
+        {
+            d=b;
+            b=c;
+            c=d+b;
+            console.log(c);
+        }
+// for(let i=1;i<=100;i++)
+// {
+//     if(i%3==0)
+//     {
+//         console.log("fizz");
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+// }
+// for(let i=1;i<=100;i++)
+//     {
+//         if(i%5==0)
+//         {
+//             console.log("buzz");
+//         }
+//         else
+//         {
+//             console.log(i);
+//         }
+//     }
