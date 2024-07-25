44776fe6d9bc0640be1abc588c4c8ef243f018e4
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-07-25 09:10:14 +0530

    added

---
+++ js/destruct.js
@@ -0,0 +1,17 @@
+// stud={name:"alan",age:20,cls:10,mrk:50}
+// const {name,age,cls,mrk}=stud;
+// console.log(stud);
+
+
+// x=[1,2,3,4,5]
+// const [a,b,c,d,e]=x;
+// console.log(x);
+
+// obj={name:"Aswin",cls:10,mark:{che:10,phy:20,math:30}}
+// const {name,cls,mark:{che,phy}}=obj
+// console.log(name,cls,che,phy);
+
+
+// a=[{name:"Aswin",age:20},20,40]
+// const[{name,age},b,c]=a;
+// console.log(name,age,b,c);
---
+++ js/spread.js
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5];
+b=[...a];
+b[0]=10;
+console.log(a);
+console.log(b);
+
+c={name:"Aswin",age:20}
+d={...c};
+d.name="Arun"
+console.log(c);
+console.log(d);
