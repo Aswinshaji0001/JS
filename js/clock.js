a58c8e62ec96a78d6876f4752b031337754b3778
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-07-25 13:23:10 +0530

    added

--- js/clock.js
+++ js/clock.js
@@ -4,9 +4,19 @@ function putZero(time){
 function timer(){
     let date = new Date();
     let hours=date.getHours()
+    let seconds=date.getSeconds()
     ampm=hours<12?"am":"pm";
-
-    document.getElementById("timer").textContent=`${putZero(hours%12)} - ${putZero(date.getMinutes())} - ${putZero(date.getSeconds())} ${ampm}`
+    if(hours<=18 && hours>=6){
+        document.getElementById("main").style.backgroundImage='url("./images/scenery-wood-rocky-fingers-dream.jpg")'
+        document.getElementById("main").style.objectFit='cover'
+        document.getElementById("main").style.backgroundSize='cover'
+    }
+    else{
+        document.getElementById("main").style.backgroundImage='url("./images/S.jpg")'
+        document.getElementById("main").style.objectFit='cover'
+        document.getElementById("main").style.backgroundSize='cover'
+    }
+    document.getElementById("timer").textContent=`${putZero(hours%12==0?hours:hours%12)} - ${putZero(date.getMinutes())} - ${putZero(date.getSeconds())} ${ampm}`
     setTimeout(()=>{
         timer()
     },1000)
