782a632150258efdf4fea0d06c4df01879f06099
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-08-08 19:18:36 +0530

    Added

--- js/flip.js
+++ js/flip.js
@@ -1,21 +1,43 @@
      async function fetchData(){
     try{
-        const res=await fetch("https://dummyjson.com/docs/products");
+        const res=await fetch("https://dummyjson.com/products");
         const products= await res.json();
-        console.log(products);
-        console.log(products.products);
         str=``
         products.products.map((product)=>{
             str+=`<div class="card">
-                    <img src="${products.thumbnail}" alt="">
-                    <h5>${products.title}</h5>
-                    <p>${products.price}</p>
+                    <img src="${product.thumbnail}" alt="">
+                    <h5>${product.title.substring(0,15)}</h5>
+                    <p>${product.price}</p>
             </div>`
         })
-        document.getElementById("cards").innerHTML=str
+        document.getElementById("cards").innerHTML=str;
     }
-    catch(error){
+    catch(error)
+    {
         console.log(error);
     }
+    document.getElementById("search").addEventListener('keyup',async(e)=>{
+        console.log(e.target.value);
+        try{
+            const res=await fetch("https://dummyjson.com/products");
+        const products= await res.json();
+        console.log(products.products);
+        str=``
+        products.products.filter((i)=>i.title.toLowerCase().includes(e.target.value.toLowerCase())).map((product)=>{
+            str+=`<div class="card">
+                    <img src="${product.thumbnail}" alt="">
+                    <h5>${product.title.substring(0,15)}</h5>
+                    <p>${product.price}</p>
+            </div>`
+        })
+        document.getElementById("cards").innerHTML=str;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+        
+    })
+    
 }
 fetchData();
